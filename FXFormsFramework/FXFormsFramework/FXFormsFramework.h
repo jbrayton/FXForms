470b4fc5d1371636620e1b481532281079a02eab
John Brayton <jbrayton@goldenhillsoftware.com>
2016-09-24 11:04:46 -0400

    Added framework for Carthage

---
+++ FXFormsFramework/FXFormsFramework/FXFormsFramework.h
@@ -0,0 +1,20 @@
+//
+//  FXFormsFramework.h
+//  FXFormsFramework
+//
+//  Created by John Brayton on 9/24/16.
+//  Copyright © 2016 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FXFormsFramework.
+FOUNDATION_EXPORT double FXFormsFrameworkVersionNumber;
+
+//! Project version string for FXFormsFramework.
+FOUNDATION_EXPORT const unsigned char FXFormsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXFormsFramework/PublicHeader.h>
+
+
+#import "FXForms.h"
